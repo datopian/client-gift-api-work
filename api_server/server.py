9e4bd3213db58e2a9d1d352722e8df7972dd6c46
steveoni <steohenoni2@gmail.com>
2021-11-22 11:32:04 +0100

    [server][m]: create api to start pipeline

--- api_server/server.py
+++ api_server/server.py
@@ -2,20 +2,47 @@ from flask import Flask
 from sqlalchemy import create_engine
 from babbage.manager import JSONCubeManager
 from babbage.api import configure_api
+from pipeline import (update_fiscal_schema, cloud_storage, 
+                      runpipeline_subcommand, generate_org )
+import logging
+import requests
+import os
 
-app = Flask('demo')
+log = logging.getLogger(__name__)
+
+app = Flask(__name__)
 engine = create_engine('postgresql://steveoni@localhost/testing')
 models_directory = 'models/'
 manager = JSONCubeManager(engine, models_directory)
 blueprint = configure_api(app, manager)
 app.register_blueprint(blueprint, url_prefix='/api/babbage/')
 
-# This code block was inserted to test whether we retrieve data from the DB
-from sqlalchemy.sql import select, text
-conn = engine.connect()
-# table name needs to be quoted so it works with dashes
-s = select([text('* FROM "5a25dc2b975eb85bb58a44a3a206d85c17b0f3fe35167d9ba535b5c15421be1"')])
-result = conn.execute(s)
-print(result.fetchone())
+@app.route('/api/pipeline/<org>')
+def pipeline(org):
+    """
+    API call to start the pipeline
+    for creating gift-api 
+    """
+    print("START API PIPELINE")
+    github_dpkg = f'https://raw.githubusercontent.com/gift-data/{org}/main/datapackage.json'
+    dpkg = requests.get(github_dpkg).json()
 
-app.run()
+    # check if organisation folder already exist
+    if (not os.path.isdir(org)):
+        generate_org(org, dpkg)
+
+    # update fiscal YAML file
+    update_fiscal_schema(org)
+
+    # download resources from google cloud
+    print("Downloading resources from Google bucket")
+    cloud_storage(dpkg, org)
+
+    run datapackage-pipeline command line
+    print("START PIPELINE SUBCOMMAND")
+    runpipeline_subcommand(org)
+
+    return "done"
+
+
+app.run(host="localhost", port=3000, debug=True)

f3381ea4a72b585cdf5117cf4ea2277613be555d
steveoni <steohenoni2@gmail.com>
2022-03-31 16:34:41 +0100

    update code base to make use of gunicorn and also add new api models

--- api_server/Readme.md
+++
@@ -1,9 +0,0 @@
-## GIFT-OPENSPENDING API
-
-For the deployment, the api must have the following enviromental variables
-
-```
-POSTGRESS_CONNECTION=A postgress db
-GOOGLE_APPLICATION_CREDENTIALS= credentials to access google cloud apis
-DPP_DB_ENGINE= same postgress dp as POSTGRESS_CONNECTION
-```
---
+++ server.py
@@ -0,0 +1,67 @@
+from flask import Flask
+from sqlalchemy import create_engine
+from babbage.manager import JSONCubeManager
+from babbage.api import configure_api
+from pipeline import (update_fiscal_schema, cloud_storage, 
+                      runpipeline_subcommand, generate_org,
+                      generate_org2, cloud_storage_openspending )
+import logging
+import requests
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+log = logging.getLogger(__name__)
+
+app = Flask(__name__)
+engine = create_engine(os.getenv('POSTGRES_CONNECTION_STR'))
+models_directory = 'models/'
+manager = JSONCubeManager(engine, models_directory)
+blueprint = configure_api(app, manager)
+app.register_blueprint(blueprint, url_prefix='/api/babbage/')
+
+@app.route('/api/pipeline/<org>')
+def pipeline(org):
+    """
+    API call to start the pipeline
+    for creating gift-api 
+    """
+    print("START API PIPELINE")
+    github_dpkg = f'https://raw.githubusercontent.com/gift-data/{org}/main/datapackage.json'
+    dpkg = requests.get(github_dpkg).json()
+
+    # check if organisation folder already exist
+    org_path = os.path.abspath(f'orgs/{org}')
+    if (not os.path.isdir(org_path)):
+        generate_org(org, dpkg)
+
+    # update fiscal YAML file
+    update_fiscal_schema(org)
+
+    # download resources from google cloud
+    print("Downloading resources from Google bucket")
+    cloud_storage(dpkg, org)
+
+    # run datapackage-pipeline command line
+    print("START PIPELINE SUBCOMMAND")
+    runpipeline_subcommand(org)
+
+    return "done"
+
+
+@app.route('/api/pipeline/openspending/<hashname>/<org>')
+def pipeline_openspending(hashname, org):
+    # Generate organization files
+    dpkg = generate_org2(org, hashname)
+    # Fetch cloud storage data
+    cloud_storage_openspending(org, hashname, dpkg)
+    # run pipe line command line
+    runpipeline_subcommand(org)
+    return "done"
+
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0')
+
+# app.run(host="localhost", port=3000, debug=True)
